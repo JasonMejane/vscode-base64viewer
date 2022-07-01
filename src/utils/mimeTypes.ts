c829ec04fa0bbbf77a8e2567299e2efef8e306c9
jmejane <51420276+JasonMejane@users.noreply.github.com>
2022-07-01 14:44:02 -0400

    Feat(#24): Support HTML/CSS/JS files

--- src/utils/mimeTypes.ts
+++ src/utils/mimeTypes.ts
@@ -6,13 +6,16 @@ export class MimeTypes {
 		this.mimeTypes = new Map<string, string>();
 		this.mimeTypes.set('avi', 'video/x-msvideo');
 		this.mimeTypes.set('bmp', 'image/bmp');
+		this.mimeTypes.set('css', 'text/css');
 		this.mimeTypes.set('doc', 'application/msword');
 		this.mimeTypes.set('docx', 'application/vnd.openxmlformats-officedocument.wordprocessingml.document');
 		this.mimeTypes.set('exe', 'application/vnd.microsoft.portable-executable');
 		this.mimeTypes.set('gif', 'image/gif');
+		this.mimeTypes.set('html', 'text/html');
 		this.mimeTypes.set('ico', 'image/x-icon');
 		this.mimeTypes.set('jpeg', 'image/jpeg');
 		this.mimeTypes.set('jpg', 'image/jpeg');
+		this.mimeTypes.set('js', 'text/javascript');
 		this.mimeTypes.set('mp3', 'audio/mpeg');
 		this.mimeTypes.set('mp4', 'video/mpeg');
 		this.mimeTypes.set('mpeg', 'video/mpeg');
@@ -29,20 +32,25 @@ export class MimeTypes {
 		this.mimeTypes.set('txt', 'text/plain');
 		this.mimeTypes.set('wav', 'audio/x-wav');
 		this.mimeTypes.set('webm', 'video/webm');
+		this.mimeTypes.set('xhtml', 'application/xhtml+xml');
 		this.mimeTypes.set('xls', 'application/vnd.ms-excel');
 		this.mimeTypes.set('xlsx', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
+		this.mimeTypes.set('xml', 'application/xml');
 		this.mimeTypes.set('zip', 'application/zip');
 
 		this.types = [
 			'avi',
 			'bmp',
+			'css',
 			'doc',
 			'docx',
 			'exe',
 			'gif',
+			'html',
 			'ico',
 			'jpeg',
 			'jpg',
+			'js',
 			'mp3',
 			'mp4',
 			'mpeg',
@@ -59,7 +67,9 @@ export class MimeTypes {
 			'txt',
 			'wav',
 			'webm',
+			'xhtml',
 			'xls',
+			'xml',
 			'xslx',
 			'zip',
 		];
